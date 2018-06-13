328803a1854ddfb5acc7892c512d57a27dd3147b
Michael Mussler <Mmussler@hotmail.com>
2018-06-13 23:28:15 +0200

    Better statistic... not perfect

--- Controllers/MyntController.cs
+++ Controllers/MyntController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Mynt.Core.Interfaces;
 using Mynt.Core.TradeManagers;
+using MyntUI.Models;
 using Newtonsoft.Json.Linq;
 
 namespace MyntUI.Controllers
@@ -78,93 +79,36 @@ namespace MyntUI.Controllers
     [Route("statistics")]
     public async Task<IActionResult> Statistic()
     {
-      var tradeOptions = Startup.Configuration.GetSection("TradeOptions").Get<TradeOptions>();
+      // Create Statistic model
+      var stat = new Statistics()
+      {
+        
+      };
 
-      // QuoteCurrency
-      ViewBag.quoteCurrency = tradeOptions.QuoteCurrency;
-
-      // MarketBlackList
-      ViewBag.quoteCurrency = tradeOptions.MarketBlackList;
-
-      // OnlyTradeList
-      ViewBag.quoteCurrency = tradeOptions.OnlyTradeList;
-
-      //  AlwaysTradeList
-      ViewBag.quoteCurrency = tradeOptions.AlwaysTradeList;
-
-      // Get closed trades
-      var closedTrades = await Globals.GlobalDataStore.GetClosedTradesAsync();
-      ViewBag.closedTrades = closedTrades;
-
+      
       // Get winner/loser currencies
       var coins = new Dictionary<string, decimal?>();
-
-      foreach (var cT in closedTrades)
+      foreach (var cT in await Globals.GlobalDataStore.GetClosedTradesAsync())
+      {
+        // Get profit per currency
         if (coins.ContainsKey(cT.Market))
           coins[cT.Market] = coins[cT.Market].Value + cT.CloseProfitPercentage;
         else
           coins.Add(cT.Market, cT.CloseProfitPercentage);
-      
-      ViewBag.profitCoins = coins.ToList().OrderByDescending(c => c.Value);
-      
 
+        // Profit-loss
+        if (cT.CloseProfit != null) stat.ProfitLoss = stat.ProfitLoss + cT.CloseProfit.Value;
+        if (cT.CloseProfitPercentage != null) stat.ProfitLossPercentage = stat.ProfitLossPercentage + cT.CloseProfitPercentage.Value;
+      }
+
+      // Coin performance
+      stat.CoinPerformance = coins.ToList().OrderByDescending(c => c.Value);
+
+      // Create some viewbags
+      ViewBag.tradeOptions = Startup.Configuration.GetSection("TradeOptions").Get<TradeOptions>();
+      ViewBag.stat = stat;
 
       return new JsonResult(ViewBag);
     }
   }
-
-  //[Route("api/mynt/test")]
-  //public class MyntTestController : Controller
-  //{
-  //}
-
-  public class MyntController : Controller
-    {
-        // GET: /<controller>/        
-        public async Task<IActionResult> Dashboard()
-        {
-            var tradeOptions = Startup.Configuration.GetSection("TradeOptions").Get<TradeOptions>();
-
-            ViewBag.quoteCurrency = tradeOptions.QuoteCurrency;
-            // Get active trades
-            var activeTrades = await Globals.GlobalDataStore.GetActiveTradesAsync();
-            ViewBag.activeTrades = activeTrades;
-
-            // Get current prices
-            //ExchangeSharp.
-
-            // Get Traders
-            var traders = await Globals.GlobalDataStore.GetTradersAsync();
-            ViewBag.traders = traders;
-
-            // Check if Trader has active trade
-            foreach (var trader in traders)
-            {
-                if (activeTrades.Count > 0)
-                {
-                    var activeTrade = activeTrades.Where(t => t.TraderId == trader.Identifier).ToList();
-                    if (activeTrade.Count >= 1)
-                    {
-                        trader.ActiveTrade = activeTrade.First();
-
-                        //Temp for shortened
-                        var actT = trader.ActiveTrade;
-
-                        // Get Tickers
-                        trader.ActiveTrade.TickerLast = await Globals.GlobalExchangeApi.GetTicker(actT.Market);
-                        trader.ActiveTrade.OpenProfit = actT.OpenRate - trader.ActiveTrade.TickerLast.Last;
-                        trader.ActiveTrade.OpenProfitPercentage = ((100 * trader.ActiveTrade.TickerLast.Last) / actT.OpenRate) - 100;
-                    }
-                }
-
-                // Check Profit/Loss
-                trader.ProfitLoss = ((100 * trader.CurrentBalance) / trader.StakeAmount) - 100;
-            }
-
-            ViewBag.closedTrades = await Globals.GlobalDataStore.GetClosedTradesAsync();
-
-            return View();
-        }
-
-    }
 }
---
+++ Models/StatisticsModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mynt.Core.Models;
+using Mynt.Core.TradeManagers;
+
+namespace MyntUI.Models
+{
+  public class Statistics
+  {
+    // Profit - loss
+    public decimal? ProfitLoss { get; set; } = 0;
+    public decimal? ProfitLossPercentage { get; set; } = 0;
+
+    //public decimal? ProfitLossToday { get; set; }
+    //public decimal? ProfitLossTodayPercentage { get; set; }
+
+    //public decimal? ProfitLossWeek { get; set; }
+    //public decimal? ProfitLossWeekPercentage { get; set; }
+
+    //public decimal? ProfitLossMonth { get; set; }
+    //public decimal? ProfitLossMonthPercentage { get; set; }
+
+    public IOrderedEnumerable<KeyValuePair<string, decimal?>> CoinPerformance { get; set; }
+  }
+}
